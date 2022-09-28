6775361fa361a19a8798a754ae7962c972030921
CIC4DA <dhruv.jain9100@gmail.com>
2022-09-29 00:18:56 +0530

    Updates

---
+++ precache-manifest.cb85445784f6a7fd55b61130352232a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "751e293b5ade0bb92b2e",
+    "url": "/TextUtils-react/static/css/main.5ef985d8.chunk.css"
+  },
+  {
+    "revision": "751e293b5ade0bb92b2e",
+    "url": "/TextUtils-react/static/js/main.751e293b.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/TextUtils-react/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/TextUtils-react/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "5207c8061e6c91bd43ca",
+    "url": "/TextUtils-react/static/js/runtime~main.5207c806.js"
+  },
+  {
+    "revision": "687716b0e86011b5cb94ffe0b3825072",
+    "url": "/TextUtils-react/index.html"
+  }
+];
